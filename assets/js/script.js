7bacdb077ae61374f19243b0c2a18a70be8c1039
Gilson511 <araujogilson831@gmail.com>
2025-06-22 22:23:38 -0300

    subindo aplication

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,32 +8,48 @@ window.onload = function () {
         }
     });
 
-    iniciarSlideAutomatico();
 };
 
 let indexAtual = 0;
+let vez = [0,1,2];
 const sliders = document.querySelector('.sliders');
 const pontos = document.querySelectorAll('.pontos');
 const banner = document.querySelector('.banner');
 
+
+setInterval(() => {
+    indexAtual++;
+    if (indexAtual > vez.length - 1) {
+        indexAtual = 0;
+
+    }
+
+    mudarSlide(indexAtual);
+}, 2000)
+
 // Mudar slide manualmente
 function mudarSlide(index) {
     indexAtual = index;
-
     pontos.forEach(ponto => ponto.classList.remove('active'));
     pontos[index].classList.add('active');
 
-    if (index === 0){
+    if (index === 0) {
         banner.style.backgroundImage = "url('../media/foto4.jpg')";
     } else {
-        if(index === 1){
-               banner.style.backgroundImage = "url('../media/foto6.jpg')";
-        }else{
-            if(index == 2){
+        if (index === 1) {
+            banner.style.backgroundImage = "url('../media/foto6.jpg')";
+        } else {
+            if (index == 2) {
                 banner.style.backgroundImage = "url('../media/foto7.jpg')"
             }
-          
+
         }
-     
+
     }
 }
+
+
+
+
+
+
