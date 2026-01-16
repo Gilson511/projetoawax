3e90f5ca27cc152ec30f5c269a56c7f457064276
Gilson511 <araujogilson831@gmail.com>
2026-01-16 19:36:52 -0300

    carrosel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,42 @@
 window.onload = function () {
-    const menuOpener = document.querySelector(".menu-opener");
-    const nav = document.querySelector(".menu nav");
+  const menuOpener = document.querySelector(".menu-opener");
+  const nav = document.querySelector(".menu nav");
 
-    if (menuOpener && nav) {
-        menuOpener.addEventListener("click", function () {
-            nav.classList.toggle("active");
-        });
-    }
+  if (menuOpener && nav) {
+    //se existir menu ativo ele pega e add o toggle ou seja o menu ativo.
+    menuOpener.addEventListener("click", function () {
+      nav.classList.toggle("active");
+    });
+  }
 };
 
 let indexAtual = 0;
-let vez = [0,1,2];
-const sliders = document.querySelector('.sliders');
-const pontos = document.querySelectorAll('.pontos');
-const banner = document.querySelector('.banner');
-
-
-setInterval(() => {
-    indexAtual++;
-    if (indexAtual > vez.length - 1) {
-        indexAtual = 0;
-
-    }
-
-    mudarSlide(indexAtual);
-}, 2000)
+let vez = [0, 1, 2];
+const sliders = document.querySelector(".banner .sliders"); //slide container
+const slide = document.querySelectorAll(".banner .slide"); //slide
+const pontos = document.querySelectorAll(".banner .pontos");
+const banner = document.querySelector(".banner");
 
 // Mudar slide manualmente
 function mudarSlide(index) {
-    indexAtual = index;
-    pontos.forEach(ponto => ponto.classList.remove('active'));
-    pontos[index].classList.add('active');
+  indexAtual = index;
+  //Move o carrosel
+  sliders.style.transform = `translateX(-${indexAtual * 100}vw)`;
 
-    if (index === 0) {
-        banner.style.backgroundImage = "url('../media/foto4.jpg')";
-    } else {
-        if (index === 1) {
-            banner.style.backgroundImage = "url('../media/foto6.jpg')";
-        } else {
-            if (index == 2) {
-                banner.style.backgroundImage = "url('../media/foto7.jpg')"
-            }
-
-        }
-
-    }
+  //aiva o ponto.
+  pontos.forEach((p) => p.classList.remove("active"));
+  pontos[index].classList.add("active");
 }
 
+setInterval(() => {
+  indexAtual++;
+  if (indexAtual >= slide.length) {
+    indexAtual = 0;
+  }
 
+  mudarSlide(indexAtual);
+}, 3000);
 
-
-
-
+pontos.forEach((ponto, index) => {
+  ponto.addEventListener("click", () => mudarSlide(index));
+});
